ede213e4e0f9c6579e70fb7f892506aa999e7b40
TonNom <ton@email.com>
2026-03-12 04:12:09 +0100

    feat: add project provider and home screen

--- lib/providers/project_provider.dart
+++ lib/providers/project_provider.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 
 class ProjectProvider extends ChangeNotifier {
 
-  List<String> _projects = [];
+  final List<String> _projects = [];
 
   List<String> get projects => _projects;
 
-  void addProject(String project){
-
-    _projects.add(project);
+  void addProject(String name){
+    _projects.add(name);
     notifyListeners();
-
   }
 
 }
